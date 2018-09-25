2bd6f5a1e43ca91637f1606eafc619c9fce4a911
germain72 <36983066+germain72@users.noreply.github.com>
2018-09-25 15:38:10 +0200

    V 1.0

---
+++ demoIB/src/main/java/com/example/demo/DemoIbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIbApplication.class, args);
+	}
+}
